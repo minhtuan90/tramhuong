23ac8b2c726f7ea601e29692f668e1e1f08a15e1
minhtuan90 <136880844+minhtuan90@users.noreply.github.com>
2026-07-02 15:39:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -157,7 +157,7 @@ if(checkoutForm) {
             quantity: document.getElementById('checkout-qty').value
         };
 
-        const scriptURL = 'https://script.google.com/macros/s/AKfycbzu5NmT8nK6eTHNhFGMKVYl4htLnWVJ5xhA5w0JJEwTyaiaPedsXNz5C9-e6NN3a8vA/exec'; // Thay link API thật vào đây
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbxKH0FER-ZtNAI9a2QDofcYdNZrGJ7jwRFWCdrm9PStMcD-a31Kx73_hXTM1aWHfCJY/exec'; // Thay link API thật vào đây
         
         setTimeout(() => {
             if(window.ttq) {
